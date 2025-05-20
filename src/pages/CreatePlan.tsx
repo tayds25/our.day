f763a72a1c3111d6a93c5448c0845e927849305c
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-05-20 09:31:50 +0800

    Started the Create Plan page and started creating the functions for the ImagePicker component

--- src/pages/CreatePlan.tsx
+++ src/pages/CreatePlan.tsx
@@ -1,5 +1,29 @@
-function CreatePlan() {
+import { useState, useRef } from 'react';
 
+const CreatePlan = () => {
+    const [image, setImage] = useState<File | null>(null);
+    const fileInputref = useRef<HTMLInputElement | null>(null);
+
+    const handleImageClick = () => {
+        fileInputref.current?.click();
+    };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0] || null;
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            setImage(imageUrl);
+        }
+    }
+
+    return (
+        // Main Container
+        <div className="flex items-center justify-center min-h-screen p-8 bg-background"> */}
+            {/* Image Rectangle */}
+            <div className=''>
+            </div>
+        </div>
+    );
 }
 
 export default CreatePlan;
