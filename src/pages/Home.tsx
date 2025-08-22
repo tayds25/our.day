ec846ed563d43161ec7be90d3c45982aa977a2dd
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-08-22 15:21:10 +0800

    Setup icons, theming, and dark/light mode toggle

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home: React.FC = () => (
+    <div className="flex items-center justify-center w-full min-h-screen bg-cream-500 dark:bg-black-500">
+        <h1 className="text-green-700 font-display text-display dark:text-pink-700">our.day</h1>
+    </div>
+);
+
+export default Home;
